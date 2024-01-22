e0886609aff845c2c51c3b1545b0099c5a143d4b
dong.huo <huodon@gmail.com>
2024-01-23 00:19:36 +0800

    bi-direcional signal bind

--- src/vitro/qwikify.tsx
+++ src/vitro/qwikify.tsx
@@ -1,10 +1,12 @@
 /* @jsxImportSource @builder.io/qwik */
 import {
 	RenderOnce,
+	Signal,
 	SkipRender,
 	Slot,
 	component$,
 	implicit$FirstArg,
+	isSignal,
 	noSerialize,
 	useSignal,
 	useStylesScoped$,
@@ -13,83 +15,142 @@ import {
 	type QRL,
 } from '@builder.io/qwik'
 
-import { isBrowser } from '@builder.io/qwik/build'
+import { isBrowser, isServer } from '@builder.io/qwik/build'
 import * as client from './client'
-import { getHostProps, main } from './slot'
+import { main, splitProps, useWakeupSignal } from './slot'
 
+import { ObservableReadonly, isObservable } from 'vitro'
 import type { FunctionComponent, QwikifyOptions, QwikifyProps } from './types'
 
 export function qwikifyQrl<PROPS extends {}>(
 	vitroCmp$: QRL<FunctionComponent<PROPS & { children?: any }>>,
 	opts?: QwikifyOptions,
 ) {
-	return component$((props: QwikifyProps<PROPS>) => {
-		const { scopeId } = useStylesScoped$(
-			`q-slot{display:none} q-slotc,q-slotc>q-slot{display:contents}`,
-		)
-		const hostRef = useSignal<Element>()
-		const slotRef = useSignal<Element>()
-		const vitroCmp = useSignal<NoSerialize<FunctionComponent<PROPS>>>()
-		const dispose = useSignal<NoSerialize<() => void>>()
-		const TagName = opts?.tagName ?? ('qwik-vitro' as any)
+	return component$(
+		(
+			props: QwikifyProps<{
+				[K in keyof PROPS]: PROPS[K] extends ObservableReadonly<infer V>
+					? Readonly<Signal<V>>
+					: PROPS[K]
+			}>,
+		) => {
+			const { scopeId } = useStylesScoped$(
+				`q-slot{display:none} q-slotc,q-slotc>q-slot{display:contents}`,
+			)
+			const hostRef = useSignal<Element>()
+			const slotRef = useSignal<Element>()
+			const vitroCmp = useSignal<NoSerialize<FunctionComponent<PROPS>>>()
+			const dispose = useSignal<NoSerialize<() => void>>()
+			const [wakeup] = useWakeupSignal(props, opts)
 
-		// Task takes cares of updates and partial hydration
-		useTask$(
-			async ({ track, cleanup }) => {
-				const trackedProps = track(() => ({ ...props }))
+			const { qwikHostProps, qwikClientProps } = splitProps(props)
 
-				if (!isBrowser) {
-					return
+			const qwikClientPropsSignal =
+				useSignal<Record<string, any>>(qwikClientProps)
+
+			const vitroPropsSignal = useSignal<Record<string, any>>()
+
+			const Host = opts?.tagName ?? ('qwik-vitro' as any)
+
+			useTask$(({ track }) => {
+				for (const key in qwikClientProps) {
+					const val = qwikClientProps[key]
+					if (isSignal(val)) {
+						track(val)
+					}
 				}
 
-				const cmp = await vitroCmp$.resolve()
+				if (isServer) return
 
-				vitroCmp.value = noSerialize(cmp)
+				const vitroProps = vitroPropsSignal.value
 
-				// Root props update
-				if (hostRef.value) {
-					dispose.value?.()
-					const disposeFn = client.render(
-						main(slotRef.value, scopeId, vitroCmp.value, trackedProps),
-						hostRef.value,
-					)
-					dispose.value = noSerialize(disposeFn)
-				}
-
-				cleanup(() => {
-					dispose.value?.()
-				})
-			},
-			{ eagerness: 'load' },
-		)
-
-		return (
-			<RenderOnce>
-				<TagName
-					{...getHostProps(props)}
-					ref={(el: Element) => {
-						if (isBrowser) {
-							hostRef.value = el
-							queueMicrotask(() => {
-								const disposeFn = client.render(
-									main(slotRef.value, scopeId, vitroCmp.value, props),
-									el,
-								)
-								dispose.value = noSerialize(disposeFn)
-							})
-						} else {
-							hostRef.value = el
+				if (!vitroProps) {
+					const props: Record<string, any> = {}
+					for (const key in qwikClientPropsSignal.value) {
+						const val = qwikClientPropsSignal.value[key]
+						props[key] = isSignal(val) ? client.signal(val.value) : val
+					}
+					vitroPropsSignal.value = noSerialize(props)
+				} else {
+					for (const key in vitroProps) {
+						const val = vitroProps[key]
+						if (isObservable(val)) {
+							val(qwikClientPropsSignal.value[key].value)
 						}
-					}}
-				>
-					{SkipRender}
-				</TagName>
-				<q-slot ref={slotRef}>
-					<Slot></Slot>
-				</q-slot>
-			</RenderOnce>
-		)
-	})
+					}
+				}
+			})
+
+			// Task takes cares of updates and partial hydration
+			useTask$(
+				async ({ track, cleanup }) => {
+					const isWakeup = track(wakeup)
+
+					if (!isBrowser) {
+						return
+					}
+
+					if (!isWakeup) return
+
+					const VitroComp = await vitroCmp$.resolve()
+
+					vitroCmp.value = noSerialize(VitroComp)
+
+					// Root props update
+					if (hostRef.value) {
+						dispose.value?.()
+						const disposeFn = client.render(
+							main(
+								slotRef.value,
+								scopeId,
+								vitroCmp.value,
+								vitroPropsSignal.value ?? {},
+								qwikClientPropsSignal,
+							),
+							hostRef.value,
+						)
+						dispose.value = noSerialize(disposeFn)
+					}
+
+					cleanup(() => {
+						dispose.value?.()
+					})
+				},
+			)
+			return (
+				<RenderOnce>
+					<Host
+						{...qwikHostProps}
+						ref={(el: Element) => {
+							if (isBrowser) {
+								hostRef.value = el
+								queueMicrotask(() => {
+									const disposeFn = client.render(
+										main(
+											slotRef.value,
+											scopeId,
+											vitroCmp.value,
+											props,
+											qwikClientPropsSignal,
+										),
+										el,
+									)
+									dispose.value = noSerialize(disposeFn)
+								})
+							} else {
+								hostRef.value = el
+							}
+						}}
+					>
+						{SkipRender}
+					</Host>
+					<q-slot ref={slotRef}>
+						<Slot></Slot>
+					</q-slot>
+				</RenderOnce>
+			)
+		},
+	)
 }
 
 export const qwikify$ = /*#__PURE__*/ implicit$FirstArg(qwikifyQrl)
--- src/vitro/slot.ts
+++ src/vitro/slot.ts
@@ -1,83 +1,122 @@
-import { $, useOnDocument, useSignal } from '@builder.io/qwik'
+import {
+	$,
+	Signal,
+	isSignal,
+	useOnDocument,
+	useSignal,
+	useTask$,
+	untrack as qwikUntrack,
+} from '@builder.io/qwik'
 import { isServer } from '@builder.io/qwik/build'
-import { createElement } from 'vitro'
+import { createElement, isObservable, untrack, useEffect } from 'vitro'
 import type { QwikifyOptions, QwikifyProps } from './types'
-export function main(
-  slotEl: Element | undefined,
-  scopeId: string,
-  RootCmp: any,
-  props: any,
-) {
-  return mainExactProps(slotEl, scopeId, RootCmp, getVitroProps(props))
-}
 
-export function mainExactProps(
-  slotEl: Element | undefined,
-  scopeId: string,
-  RootCmp: any,
-  props: any,
+export function main(
+	slotEl: Element | undefined,
+	scopeId: string,
+	RootCmp: any,
+	props: any,
+	qwikClientPropsSignal: Signal<Record<string, any>>,
 ) {
-  return createElement(RootCmp, {
-    ...props,
-    children: createElement(SlotElement, {
-      slotEl,
-      scopeId,
-    }),
-  })
+	useEffect(() => {
+		for (const key in props) {
+			const val = props[key]
+
+			if (isObservable(val)) {
+				const qv = qwikUntrack(() => qwikClientPropsSignal.value[key].value)
+				const vv = val()
+				if (qv !== vv) {
+					qwikClientPropsSignal.value[key].value = vv
+				}
+			}
+		}
+	})
+	return untrack(() => {
+		return createElement(RootCmp, {
+			...props,
+			children: createElement(SlotElement, {
+				slotEl,
+				scopeId,
+			}),
+		})
+	})
 }
 const SlotElement = (props: {
-  slotEl: Element | undefined
-  scopeId: string
+	slotEl: Element | undefined
+	scopeId: string
 }) => {
-  const mount = (el: Element) => {
-    props.slotEl && el.appendChild(props.slotEl)
-  }
+	const mount = (el: Element) => {
+		props.slotEl && el.appendChild(props.slotEl)
+	}
 
-  // @ts-expect-error since q-slotc is not a standard element, we will get error
-  return createElement('q-slotc', {
-    class: props.scopeId,
-    suppressHydrationWarning: true,
-    dangerouslySetInnerHTML: { __html: '<!--SLOT-->' },
-    ref: mount,
-  })
+	// @ts-expect-error since q-slotc is not a standard element, we will get error
+	return createElement('q-slotc', {
+		class: props.scopeId,
+		suppressHydrationWarning: true,
+		dangerouslySetInnerHTML: { __html: '<!--SLOT-->' },
+		ref: mount,
+	})
 }
 
 export const getVitroProps = (
-  props: Record<string, any>,
+	props: Record<string, any>,
 ): Record<string, any> => {
-  const obj: Record<string, any> = {}
-  Object.keys(props).forEach((key) => {
-    if (!key.startsWith(HOST_PREFIX)) {
-      const normalizedKey = key.endsWith('$') ? key.slice(0, -1) : key
-      obj[normalizedKey] = props[key]
-    }
-  })
-  return obj
+	const obj: Record<string, any> = {}
+	Object.keys(props).forEach((key) => {
+		if (!key.startsWith(HOST_PREFIX)) {
+			const normalizedKey = key.endsWith('$') ? key.slice(0, -1) : key
+			obj[normalizedKey] = props[key]
+		}
+	})
+	return obj
 }
 
-export const getHostProps = (
-  props: Record<string, any>,
-): Record<string, any> => {
-  const obj: Record<string, any> = {}
-  Object.keys(props).forEach((key) => {
-    if (key.startsWith(HOST_PREFIX)) {
-      obj[key.slice(HOST_PREFIX.length)] = props[key]
-    }
-  })
-  return obj
+export const splitProps = (props: Record<string, any>) => {
+	const qwikHostProps: Record<string, any> = {}
+	const qwikClientProps: Record<string, any> = {}
+
+	for (const key in props) {
+		const val = props[key]
+		if (key.startsWith(HOST_PREFIX)) {
+			qwikHostProps[key.slice(HOST_PREFIX.length)] = val
+		} else {
+			if (!key.startsWith(HOST_PREFIX) && !key.startsWith('client:')) {
+				if (isSignal(val)) {
+					qwikClientProps[key] = val
+				}
+
+				const normalizedKey = key.endsWith('$') ? key.slice(0, -1) : key
+				qwikClientProps[normalizedKey] = val
+			}
+		}
+	}
+	return { qwikHostProps, qwikClientProps }
 }
 
 export const useWakeupSignal = (
-  props: QwikifyProps<{}>,
-  opts: QwikifyOptions = {},
+	props: QwikifyProps<{}>,
+	opts: QwikifyOptions = {},
 ) => {
-  const signal = useSignal(false)
-  const activate = $(() => (signal.value = true))
-  if (isServer) {
-    // vitro component is client-only
-    useOnDocument('qinit', activate)
-  }
-  return [signal, activate] as const
+	const signal = useSignal(false)
+	const activate = $(() => (signal.value = true))
+	if (isServer) {
+		if (props['client:load']) {
+			useOnDocument('qinit', activate)
+		} else if (props['client:idle']) {
+			useOnDocument('qidle', activate)
+		}
+
+		const wakeup = props['client:signal']
+		if (wakeup) {
+			useTask$(({ track }) => {
+				const isWakeup = track(wakeup)
+				if (isWakeup) {
+					signal.value = true
+				}
+			})
+		}
+	}
+	return [signal, activate] as const
 }
 
 const HOST_PREFIX = 'host:'
